dc996e834825608ff3c599b910fca8c9b1d99707
Aditya Bhutame <bhutame@gmail.com>
2025-09-14 00:15:40 +0530

    Removed nested repo Project 1 before submission

---
+++ OneDrive/Desktop/CS50's Webdev/Python/1.py
@@ -0,0 +1,99 @@
+# name = (input("Enter your name: "))
+# print("Hello, " +name)
+# age = (input("Enter the age: "))
+# print("Your age is " + age)
+
+
+#Conditions
+# num = int(input("Enter the number: "))
+# if num > 0:
+#     print("The number is positive")
+# elif num < 0:
+#     print("The number is negative")
+# else:
+#     print("The number is zero")
+
+# num3 = int(input("Enter the number: "))
+# while(num3 < 18):
+#     print("You are not eligible to vote")
+# print("You are eligible to vote")
+
+
+# num1 = (input("Enter the first number: "))
+# num2 = (input("Enter the second number:"))
+# sum = num1 + num2
+# print("The sum of num1 and num2 is: " +sum)
+
+
+#Sequences
+#Strings
+# str = "Hello"
+# print(str[0])
+# print(str[1])
+# print(str[2])
+# print(str[3])
+# print(str[4])
+
+# str1 = (input("Enter the string: "))
+# print("Character at index 2 is:" +str1[2])
+
+
+#Lists
+# names = ["Radha","Krishna","Ram","Sita","Ravan"]
+# print(names)
+# print(names[0])
+# print(names[1]) 
+# names.append("Hanuman")
+# print(names)
+# names.remove("Ravan")
+# print(names)
+# names.sort()
+# print(names)
+
+
+#Sets
+# s = set()
+# s.add(1)
+# s.add(2)    
+# s.add(3)
+# s.add(2)  # Duplicate, will not be added
+# print(s)
+# s.remove(2)
+# print(s)
+# print("The set has ", len(s),  "elements.")
+# s.clear()
+# print(s)
+
+#Dictionaries
+# vehicals = {"car": "BMW", "bike": "Discover", "cycle": "Hercules"}
+# print(vehicals)
+# print(vehicals["car"])
+
+
+#Loops
+# for i in range(5):
+#     print(i)
+
+# for j in range(2, 10):
+#     print(j)
+
+# for k in range(1, 10, 3):
+#     print(k)
+
+
+#Functions
+# def square(x):
+#     return x * x
+# for i in range(1, 6):
+#     print("The square of ", i, "is", square(i))
+
+# def factorial(m):
+#     if m == 0 or m == 1:
+#         return 1
+#     else:
+#         return m*factorial(m-1)
+# num4 = int(input("Enter a number: "))
+# result = factorial(num4)
+# print("The factorial of", num4, "is", result)
+
+
---
+++ OneDrive/Desktop/CS50's Webdev/Python/2.py
@@ -0,0 +1,8 @@
+#Modules
+from functions import square
+
+for i in range(1, 6):
+    print("The square of ", i, "is", square(i))
+
+import functions
+print(functions.square(7))
